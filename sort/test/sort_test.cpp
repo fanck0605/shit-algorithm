559037b842875621f5c553ec62af4f1084af2b96
Chuck Fan <fanck0605@qq.com>
2021-10-24 22:07:52 +0800

    sort test: cassert

--- sort/test/sort_test.cpp
+++ sort/test/sort_test.cpp
@@ -1,43 +1,57 @@
 #include <iostream>
+#include <algorithm>
+#include <cassert>
 #include "sort.h"
 
 int main()
 {
-    std::vector a{ 16,4,10,14,1,9,3,2,8,7 };
+    std::vector toSort{ 16,4,10,14,1,9,3,2,8,7 };
+    std::vector sorted = toSort;
+    std::sort(sorted.begin(), sorted.end());
 
     std::cout << "to sort: ";
-    for (auto i : a)
+    for (auto i : toSort)
     {
         std::cout << i << ' ';
     }
+
+    std::vector<int> result;
 
     std::cout << "\ninsert sort: ";
 
-    for (auto i : insertSort(a))
+    result = insertSort(toSort);
+    for (auto i : result)
     {
         std::cout << i << ' ';
     }
+    assert(result == sorted);
 
     std::cout << "\nbubble sort: ";
 
-    for (auto i : bubbleSort(a))
+    result = bubbleSort(toSort);
+    for (auto i : result)
     {
         std::cout << i << ' ';
     }
+    assert(result == sorted);
 
     std::cout << "\nquick sort: ";
 
-    for (auto i : quickSort(a))
+    result = quickSort(toSort);
+    for (auto i : result)
     {
         std::cout << i << ' ';
     }
+    assert(result == sorted);
 
     std::cout << "\nheap sort: ";
 
-    for (auto i : heapSort(a))
+    result = heapSort(toSort);
+    for (auto i : result)
     {
         std::cout << i << ' ';
     }
+    assert(result == sorted);
 
     std::cout << std::endl;
     return 0;
